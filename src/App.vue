96a65cc6618fb4c4980fcf574011756cf8fb0734
SalinaPrajapati <saleenaprajapati1@gmai.com>
2022-06-16 23:16:36 +0545

    erojob brand

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CompanyBrand />>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CompanyBrand from './components/CompanyBrand.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   CompanyBrand
   }
 }
 </script>
---
+++ src/components/CompanyBrand.vue
@@ -0,0 +1,158 @@
+<template>
+     <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            <div class="text-center lg:w-2/3 w-full">
+            <h2 class="title-font sm:text-3xl text-2xl mb-4 font-bold text-gray-900">Almost 28 million
+                people live in Nepal,
+                with a labour force 
+                of 11.7 million. 
+                Every year 300,000 
+                to 400,000 skilled 
+                people are in search 
+                of a job.</h2>
+            <p class=" leading-relaxed">Yet many people are unemployed or under employed. 
+                They don’t have access to appropriate employment 
+                opportunities because they are unaware of where 
+                to look. Or – they are over qualified and therefore 
+                under satisfied in their current work role. The result 
+                – Unemployment, foreign labour migration, low job 
+                satisfaction and employee retention.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-16 mx-auto">
+            <div class="flex flex-col text-center w-full mb-16">
+                <h2 class="sm:text-3xl text-2xl font-bold title-font text-gray-900">merojob exists  to fill this gap</h2>
+            </div>
+            <div class="flex flex-wrap -m-4">
+            <div class="p-4 md:w-1/3">
+                <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div class="flex items-center mb-3">
+                    <h2 class="text-gray-900 text-lg title-font font-medium">We create</h2>
+                </div>
+                <div class="flex-grow">
+                    <p class="leading-relaxed text-base">access to opportunity for all Nepalis to build their career</p>
+                </div>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/3">
+                <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div class="flex items-center mb-3">
+                    <h2 class="text-gray-900 text-lg title-font font-medium">Preparing</h2>
+                </div>
+                <div class="flex-grow">
+                    <p class="leading-relaxed text-base">clients for the journey of education, work, and life empowering them to learn for life.</p>
+                </div>
+                </div>
+            </div>
+            <div class="p-4 md:w-1/3">
+                <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                <div class="flex items-center mb-3">
+                    <h2 class="text-gray-900 text-lg title-font font-medium">By providing</h2>
+                </div>
+                <div class="flex-grow">
+                    <p class="leading-relaxed text-base">them with support, and skills development that they need to thrive.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-16">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">We aspire to create the next generation of learners, <br> leaders and citizens; empowering all who we serve</h1>
+            </div>
+            <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/2 sm:w-1/2 p-4">
+                <div class="flex relative">
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="@/assets/images/better.jpg">
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Better together</h2>
+                    <p class="leading-relaxed">We embody a spirit of partnership, and collaboration; together, we hope to create a culture of success.</p>
+                </div>
+                </div>
+            </div>
+            <div class="lg:w-1/2 sm:w-1/2 p-4">
+                <div class="flex relative">
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="@/assets/images/commitment.jpg">
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Commitment</h2>
+                    <p class="leading-relaxed">We demonstrate tangible impact and enable long-term change through providing employment opportunity in the region</p>
+                </div>
+                </div>
+            </div>
+            <div class="lg:w-1/2 sm:w-1/2 p-4">
+                <div class="flex relative">
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="@/assets/images/excellence.jpg">
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Excellence</h2>
+                    <p class="leading-relaxed">We strive to set the highest standards for ourselves, our clients and our partners</p>
+                </div>
+                </div>
+            </div>
+            <div class="lg:w-1/2 sm:w-1/2 p-4">
+                <div class="flex relative">
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="@/assets/images/innovation.jpg">
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                    <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Innovation</h2>
+                    <p class="leading-relaxed">We think in terms of big ideas and are unafraid to employ new ways of approaching professional development</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="lg:w-4/5 mx-auto flex flex-wrap">
+                <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="@/assets/images/mount.png">
+                <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                  <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="@/assets/images/mjicons.png">  
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font overflow-hidden bg-blue-900">
+        <div class="container px-5 py-16 mx-auto">
+            <div class="flex flex-wrap -m-12">
+            <div class="p-12 md:w-1/2 flex flex-col items-start text-white">
+                <h3 class="sm:text-2xl text-lg title-font font-medium mt-4 mb-4">We imagine our organization 
+                as being a central connector, 
+                bringing together two disparate 
+                things that may not have 
+                connected otherwise.</h3>
+            </div>
+            <div class="p-12 md:w-1/2 flex flex-col items-start text-white">
+                <h3 class="sm:text-2xl text-lg title-font font-medium mt-4 mb-4">Potential comes alive at this 
+                central point of connection, 
+                with the icon representing
+                a platform for action</h3>
+            </div>
+            </div>
+        </div>
+        <div class="mb-6">
+            <img class="mx-auto h-40" src="@/assets/images/connect.png" alt="">
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-col">
+            <div class="lg:w-4/6 mx-auto">
+                <div class="sm:w-2/3 sm:pl-8 sm:py-8 mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                    <p class="font-black">Brandmark</p>
+                    <p class="leading-relaxed text-lg mb-4">The brandmark is composed of three integral parts; icon, wordmark, tagline</p>
+                </div>
+                <div class="rounded-lg">
+                    <img alt="content" class=" h-full w-full" src="@/assets/images/flow.png">
+                </div>
+            </div>
+        </div>
+    </section>
+   
+</template>
